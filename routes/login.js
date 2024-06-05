5236adcdf7dc61e67d601bb5867140144eb9b52b
JayPicksLee <jpicksley@outlook.com>
2024-06-05 14:23:12 +0100

    Basic register and login implemented

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const usermodel = require('../model/users.js');
 
 //GET METHOD: Rendering login page upon site start up
 router.get('/', function(req, res, next) { 
+    console.log(req.session);
+    console.log(req.session.id);
+  
+    req.sessionStore.get(req.session.id, (err, sessionData) =>{
+      if(err){
+        console.log(err);
+        throw err;
+      }
+      console.log(sessionData);
+    });
+  
+    //Tracking if the visitor has visited the website before, normally the session id resets upon every visit to the main page. Now we can watch the visitor and what they do.
+    req.session.visited = true;
+  
   res.render('login');
 });
 
 //POST METHOD login: Logging in the user by checking if the user first exists, and then checking the users login details (username, password). The users account level is then checked in order to direct them to the correct page.
 router.post(
-    '/login', 
+    '/loginRequest', 
     async (req, res) => {
 
     const username = req.body.username;
@@ -55,6 +70,7 @@ router.post(
       res.render('index', { error: true, message: "An error occurred" });
     }
     
+
   });
 
 
