201d83ec7f1f13dd82fe25e6b84fcf0738f2444e
JayPicksLee <jpicksley@outlook.com>
2024-06-05 13:54:11 +0100

    Bcrypt implementation, user implementation

---
+++ models/users.js
@@ -0,0 +1,141 @@
+var mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    accountLevel: Boolean,
+    username: String,
+    password: String,
+    email: String,
+    phoneNumber: String,
+});
+
+const User = mongoose.model("User", UserSchema)
+
+exports.getUsers= async ()=>{
+    const all = await User.find({});
+
+    return all;
+}
+
+exports.getUser= async (username)=>{
+    const user = await User.findOne({username:username})
+
+    return user;
+}
+
+exports.getUserID=async (username)=>{
+    const id = await User.findOne({username: username})
+
+    return id.id;
+}
+
+exports.getUserStatus=async (username)=>{
+    const level = await User.findOne({username: username})
+
+    return level.accountLevel;
+}
+
+exports.checkExists = async (username)=>{
+    try {
+
+        let user = await User.findOne({username: username})
+        let check = false;
+
+        if(user !== null){
+            check = true;
+            return check
+        }else{
+            return check;
+        }
+        } catch (error) {
+            throw new Error("Error finding username: " +error.message);
+    }
+};
+
+exports.checkLoginDetails = async (username, password)=>{
+
+    try {
+        
+        let user = await User.findOne({username: username})
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return false;
+        }        
+
+        return user;
+    } catch (error) {
+        throw new Error("Error checking login details: " +error.message);
+    }
+};
+
+
+exports.signUpUser = async (username, password, email, phoneNumber) => 
+    {
+    try 
+    {
+        const isAdmin = false;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const phoneRegex = /^\d{11}$/;
+
+        const userExists = await User.findOne({username: username})
+
+        let check = false;
+
+        const passwordHashed = await bcrypt.hash(password, 8);
+
+        if(userExists !== null){
+            check = true
+        }else{
+            check = false;
+        }
+        console.log(check);
+
+        if (check) 
+        {
+            console.log('Username already exists');
+            return;
+        }
+        if (!emailRegex.test(email)) 
+            {
+            console.log('Invalid email format, please ensure it agrees with this example, example@example.com or .co.uk etc');
+            return;
+        }
+        if (!phoneRegex.test(phoneNumber)) 
+        {
+            console.log('Invalid phone number format, ensure it is 11 digits long, it may need to start with a 0');
+            return;
+        }
+            const newUser = new User({accountLevel: isAdmin, username: username, password: passwordHashed, email: email, phoneNumber: phoneNumber});
+            const savedUser = await newUser.save();
+    } 
+    catch (error) 
+    {
+        console.log('Error saving user data: ' + error.message);
+    }
+}
+
+exports.displayUserAccounts = async () => 
+    {
+        try 
+        {
+            return await User.find({ accountLevel: false }, { _id: 1, username: 1, email: 1, phoneNumber: 1 });
+        } 
+        catch (error)
+        {
+            throw error;
+        }
+    }
+
+exports.deleteUserAccount = async (userId) =>
+    {
+        try
+        {
+            return await User.deleteOne({ _id: userId });
+        }
+        catch (error)
+        {
+            throw error;
+        }
+    }
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,61 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+//GET METHOD: Rendering login page upon site start up
+router.get('/', function(req, res, next) { 
   res.render('login');
 });
+
+//POST METHOD login: Logging in the user by checking if the user first exists, and then checking the users login details (username, password). The users account level is then checked in order to direct them to the correct page.
+router.post(
+    '/login', 
+    async (req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    try {
+      //Checking if the username inputted exists
+      const exists = await usermodel.checkExists(username);
+  
+      if (exists) {
+  
+        const userID = await usermodel.getUserID(username);
+        req.session.userID = userID;
+        
+        //Checking user details
+        const user = await usermodel.checkLoginDetails(username, password);
+  
+        if (user) {
+          
+          const level = await usermodel.getUserStatus(username);
+          //Checking user status to differentiate user and admin
+          if (level) { 
+  
+            res.redirect('/mainAdmin');
+          } else {
+  
+            res.redirect('/main');
+          }
+  
+        } else {
+  
+          console.log("Invalid login details");
+          res.render('index', { error: true, message: "Invalid login details" });
+        }
+      } else {
+  
+        console.log("User does not exist");
+        res.render('index', { error: true, message: "User does not exist" });
+      }
+  
+    } catch (error) {
+  
+      console.error("An error occurred:", error);
+      res.render('index', { error: true, message: "An error occurred" });
+    }
+    
+  });
+
 
 module.exports = router;
