82d64f4bc62b2c29f0d8070557940ac0bd7da2a7
JayPicksLee <jpicksley@outlook.com>
2024-06-08 20:50:21 +0100

    Admin page dialog box for adding items implemented

---
+++ public/javascripts/adminCS.js
@@ -0,0 +1,8 @@
+
+const marketItemDialog = document.getElementById("newItemDialog")
+
+function showNewMarketItemDialog(){
+    console.log("HELLOO");
+    console.log(marketItemDialog);
+    marketItemDialog.showModal();
+}
