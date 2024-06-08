0fe33dec866f4533be9f455b0c37f1f4856e212a
BookyDis <wnaphon@gmail.com>
2024-06-08 23:38:03 +0100

    added dynamic rendering of market items, added one photo

--- routes/market.js
+++ routes/market.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const marketmodel = require('../model/marketitem.js');
 
 /* GET market page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   console.log(req.session);
   console.log(req.session.id);
   console.log(req.session.userID);
@@ -18,7 +19,9 @@ router.get('/', function(req, res, next) {
 
   //Tracking if the visitor has visited the website before, normally the session id resets upon every visit to the main page. Now we can watch the visitor and what they do.
   req.session.visited = true;
-  res.render('market', { title: 'Market'});
+  let items = await marketmodel.getItems();
+  console.log(items)
+  res.render('market', { title: 'Market', items: items});
 });
 
 module.exports = router;
