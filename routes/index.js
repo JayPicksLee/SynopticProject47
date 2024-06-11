677b8e67e7662f7d7fdc99bf6c252d371e176546
JayPicksLee <jpicksley@outlook.com>
2024-06-11 17:56:25 +0100

    Working on language translation

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,26 @@ router.get('/', function(req, res, next) {
 
   res.render('index', { title: 'Pu Ngaol, Here and Now' });
 });
+//GET REQUEST th: For the thai version of the website
+router.get('/th', function(req, res, next) {
+  console.log(req.session);
+  console.log(req.session.id);
+  console.log(req.session.userID);
+  console.log(req.session.isLoggedIn);
+
+  req.sessionStore.get(req.session.id, (err, sessionData) =>{
+    if(err){
+      console.log(err);
+      throw err;
+    }
+    console.log(sessionData);
+  });
+
+  //Tracking if the visitor has visited the website before, normally the session id resets upon every visit to the main page. Now we can watch the visitor and what they do.
+  req.session.visited = true;
+
+  res.render('thaiindex', { title: 'Pu Ngaol, Here and Now' });
+});
 
 //POST METHOD logout: method is used for all pages, upon post request from any page from signout button, destroy the current session.
 router.post(
--- routes/market.js
+++ routes/market.js
@@ -24,4 +24,26 @@ router.get('/', async function(req, res, next) {
   res.render('market', { title: 'Market', items: items});
 });
 
+/* GET market page. */
+router.get('/th/market', async function(req, res, next) {
+  console.log(req.session);
+  console.log(req.session.id);
+  console.log(req.session.userID);
+  console.log(req.session.isLoggedIn);
+
+  req.sessionStore.get(req.session.id, (err, sessionData) =>{
+    if(err){
+      console.log(err);
+      throw err;
+    }
+    console.log(sessionData);
+  });
+
+  //Tracking if the visitor has visited the website before, normally the session id resets upon every visit to the main page. Now we can watch the visitor and what they do.
+  req.session.visited = true;
+  let items = await marketmodel.getItems();
+  console.log(items)
+  res.render('thaimarket', { title: 'Market', items: items});
+});
+
 module.exports = router;
